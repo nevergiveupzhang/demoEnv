ec9201505791e9aa127ef1685e0cff0d42ab1fb0
nevergiveupzhang <zhtao01@163.com>
2020-01-17 00:06:30 +0800

    20200116

---
+++ casClientDemo/src/main/java/com/example/demo/filter/LinkFilter.java
@@ -0,0 +1,54 @@
+package com.example.demo.filter;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.lang.StringUtils;
+import org.jasig.cas.client.validation.AssertionImpl;
+
+import com.alibaba.fastjson.JSONObject;
+import com.example.demo.utils.HttpUtils;
+
+public class LinkFilter implements Filter {
+
+	@Override
+	public void init(FilterConfig filterConfig) throws ServletException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		for(Cookie cookie:((HttpServletRequest)request).getCookies()) {
+			if("test".equals(cookie.getName())){
+				System.out.println("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+			}
+		}
+//		((HttpServletResponse)response).addCookie(new Cookie("test","test"));
+		String url="http://localhost:8219/casServerDemo/v1/tickets/";
+		String tgtRes=HttpUtils.httpPost4Tgt(url, "username=test&password=test");
+		System.out.println(tgtRes);
+		String st=HttpUtils.httpPost4St(url+tgtRes,"service=http://localhost:8219/casClientDemo/");
+		if(StringUtils.isNotBlank(st)) {
+			((HttpServletRequest)request).getSession().setAttribute("_const_cas_assertion_", new AssertionImpl(""));
+		}
+		System.out.println(st);
+		chain.doFilter(request, response);
+	}
+
+	@Override
+	public void destroy() {
+		
+	}
+
+}
--- casServerDemo/src/main/java/com/example/demo/service/TestQueryAndEncodeDatabaseAuthenticationHandler.java
+++ casServerDemo/src/main/java/com/example/demo/service/TestQueryAndEncodeDatabaseAuthenticationHandler.java
@@ -8,15 +8,17 @@ import javax.sql.DataSource;
 import javax.validation.constraints.NotNull;
 
 import org.jasig.cas.adaptors.jdbc.AbstractJdbcUsernamePasswordAuthenticationHandler;
+import org.jasig.cas.authentication.AnyAuthenticationPolicy;
 import org.jasig.cas.authentication.HandlerResult;
+import org.jasig.cas.authentication.PolicyBasedAuthenticationManager;
 import org.jasig.cas.authentication.PreventedException;
+import org.jasig.cas.authentication.RequiredHandlerAuthenticationPolicy;
 import org.jasig.cas.authentication.UsernamePasswordCredential;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 
 public class TestQueryAndEncodeDatabaseAuthenticationHandler extends AbstractJdbcUsernamePasswordAuthenticationHandler{
-
 	@Override
 	protected HandlerResult authenticateUsernamePasswordInternal(UsernamePasswordCredential transformedCredential)
 			throws GeneralSecurityException, PreventedException {
