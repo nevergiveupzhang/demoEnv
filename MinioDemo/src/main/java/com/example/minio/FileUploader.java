69ded17ed530bec1cee8d0d3d1fb3f7cc63461d0
nevergiveupzhang <zhtao01@163.com>
2020-11-01 20:49:40 +0800

    修改minio java client api版本

--- MinioDemo/src/main/java/com/example/minio/FileUploader.java
+++ MinioDemo/src/main/java/com/example/minio/FileUploader.java
@@ -1,12 +1,10 @@
 package com.example.minio;
 
-import io.minio.MinioClient;
-import io.minio.Result;
+import com.sun.javaws.exceptions.InvalidArgumentException;
+import io.minio.*;
 import io.minio.errors.*;
 import io.minio.messages.Item;
-import io.minio.policy.PolicyType;
 import org.apache.log4j.Logger;
-import org.xmlpull.v1.XmlPullParserException;
 
 import java.io.*;
 import java.security.InvalidKeyException;
@@ -25,17 +23,11 @@ public class FileUploader {
 //    private final static String TEST_PNG_FILE ="E:\\Tmp\\test.png";
     static {
         // 使用MinIO服务的URL，端口，Access key和Secret key创建一个MinioClient对象
-        try {
-            minioClient = new MinioClient(url, "minioadmin", "minioadmin");
+        minioClient = new MinioClient(url, "minioadmin", "minioadmin");
 //            minioClient = new MinioClient(url);
-        } catch (InvalidEndpointException e) {
-            e.printStackTrace();
-        } catch (InvalidPortException e) {
-            e.printStackTrace();
-        }
 
     }
-    public static void main(String[] args) throws NoSuchAlgorithmException, IOException, InvalidKeyException, XmlPullParserException, InsufficientDataException, InternalException, NoResponseException, RegionConflictException, ErrorResponseException, InvalidArgumentException, InvalidBucketNameException, InvalidObjectPrefixException {
+    public static void main(String[] args) throws NoSuchAlgorithmException, IOException, InvalidKeyException, InsufficientDataException, InternalException, RegionConflictException, ErrorResponseException, InvalidArgumentException, InvalidBucketNameException, XmlParserException, ServerException, InvalidResponseException {
 //        putObject();
 //        putObject("test");
 //        putObject("test2","test1.png");
@@ -57,9 +49,9 @@ public class FileUploader {
 //        download();
     }
 
-    private static void download() throws IOException, InvalidKeyException, NoSuchAlgorithmException, InsufficientDataException, InternalException, NoResponseException, InvalidBucketNameException, XmlPullParserException, ErrorResponseException {
+    private static void download() throws IOException, InvalidKeyException, NoSuchAlgorithmException, InsufficientDataException, InternalException, InvalidBucketNameException, ErrorResponseException, XmlParserException, ServerException, InvalidResponseException {
         String bucketName = "test";
-        if(!minioClient.bucketExists(bucketName)){
+        if(!bucketExists(bucketName)){
             return;
         }
         long start = System.currentTimeMillis();
@@ -78,23 +70,23 @@ public class FileUploader {
         System.out.println(System.currentTimeMillis() - start);
     }
 
-    private static void removeObject() throws IOException, InvalidKeyException, NoSuchAlgorithmException, InsufficientDataException, InternalException, NoResponseException, InvalidBucketNameException, XmlPullParserException, ErrorResponseException {
+    private static void removeObject() throws IOException, InvalidKeyException, NoSuchAlgorithmException, InsufficientDataException, InternalException, InvalidBucketNameException, ErrorResponseException, XmlParserException, ServerException, InvalidResponseException {
         String bucketName = "test";
-        if(!minioClient.bucketExists(bucketName)){
+        if(!minioClient.bucketExists(BucketExistsArgs.builder().bucket(bucketName).build())){
             return;
         }
         long start = System.currentTimeMillis();
         for(int i=1;i<=5000;i++){
-            minioClient.removeObject(bucketName,bucketName+i+"-binlog");
+            minioClient.removeObject(RemoveObjectArgs.builder().bucket(bucketName).object(bucketName+i+"-binlog").build());
         }
         System.out.println(System.currentTimeMillis() - start);
     }
 
-    private static void upload() throws IOException, InvalidKeyException, NoSuchAlgorithmException, InsufficientDataException, InternalException, NoResponseException, InvalidBucketNameException, XmlPullParserException, ErrorResponseException, RegionConflictException, InvalidArgumentException {
+    private static void upload() throws IOException, InvalidKeyException, NoSuchAlgorithmException, InsufficientDataException, InternalException, InvalidBucketNameException, ErrorResponseException, RegionConflictException, InvalidArgumentException, XmlParserException, ServerException, InvalidResponseException {
         String bucketNamePrefix = "test";
         String bucketName = "test";
-        if(!minioClient.bucketExists(bucketName)){
-            minioClient.makeBucket(bucketName);
+        if(!bucketExists(bucketName)){
+            makeBucket(bucketName);
         }
 
         long start = System.currentTimeMillis();
@@ -102,10 +94,14 @@ public class FileUploader {
             if(i%5000 == 1){
                 log.info("第"+(i/5000 + 1)+"个5000开始时间："+new Date());
             }
-            minioClient.putObject(bucketName,bucketNamePrefix+i+".png",TEST_PNG_FILE);
+            uploadObject(bucketName,bucketNamePrefix+i+".png",TEST_PNG_FILE);
         }
         log.info("结束时间："+new Date());
         log.info(System.currentTimeMillis() - start);
+    }
+
+    private static void makeBucket(String bucketName) throws IOException, InvalidKeyException, InvalidResponseException, RegionConflictException, InsufficientDataException, NoSuchAlgorithmException, ServerException, InternalException, XmlParserException, InvalidBucketNameException, ErrorResponseException {
+        minioClient.makeBucket(MakeBucketArgs.builder().bucket(bucketName).build());
     }
 
     private static void upload(String rootPath) {
@@ -131,10 +127,10 @@ public class FileUploader {
         String currentFileName = currentFilePath.substring(currentFilePath.lastIndexOf(File.separator)+1).replace("@","-");
         if(currentFile.isFile()){
             try {
-                if(!minioClient.bucketExists(bucketName)){
-                    minioClient.makeBucket(bucketName);
+                if(!bucketExists(bucketName)){
+                    makeBucket(bucketName);
                 }
-                minioClient.putObject(bucketName,currentFileName,currentFilePath);
+                uploadObject(bucketName,currentFileName,currentFilePath);
             } catch (Exception e) {
                 e.printStackTrace();
             }
@@ -150,37 +146,41 @@ public class FileUploader {
         }
     }
 
-    private static void bucketPolicy() throws IOException, InvalidKeyException, NoSuchAlgorithmException, InsufficientDataException, ErrorResponseException, NoResponseException, InvalidBucketNameException, XmlPullParserException, InternalException, RegionConflictException, InvalidArgumentException, InvalidObjectPrefixException {
-        minioClient.makeBucket("test");
-        minioClient.putObject("test","test.png",TEST_PNG_FILE);
-        minioClient.setBucketPolicy("test","test", PolicyType.READ_ONLY);
+    private static void bucketPolicy() throws IOException, InvalidKeyException, NoSuchAlgorithmException, InsufficientDataException, ErrorResponseException,  InvalidBucketNameException,  InternalException, RegionConflictException, InvalidArgumentException {
+//        makeBucket("test");
+//        uploadObject("test","test.png",TEST_PNG_FILE);
+//        minioClient.setBucketPolicy("test","test", PolicyType.READ_ONLY);
     }
 
-    private static void forceCreateBucket(String bucketName) throws IOException, InvalidKeyException, NoSuchAlgorithmException, InsufficientDataException, InternalException, NoResponseException, InvalidBucketNameException, XmlPullParserException, ErrorResponseException, RegionConflictException {
-        minioClient.makeBucket(bucketName);
+    private static void forceCreateBucket(String bucketName) throws IOException, InvalidKeyException, NoSuchAlgorithmException, InsufficientDataException, InternalException,  InvalidBucketNameException,  ErrorResponseException, RegionConflictException, XmlParserException, ServerException, InvalidResponseException {
+        makeBucket(bucketName);
         System.out.println(bucketName + " Bucket created.");
     }
 
-    private static void createBucket(String bucketName) throws IOException, InvalidKeyException, NoSuchAlgorithmException, InsufficientDataException, InternalException, NoResponseException, InvalidBucketNameException, XmlPullParserException, ErrorResponseException, RegionConflictException {
-        boolean isExist = minioClient.bucketExists(bucketName);
+    private static void createBucket(String bucketName) throws IOException, InvalidKeyException, NoSuchAlgorithmException, InsufficientDataException, InternalException,  InvalidBucketNameException,  ErrorResponseException, RegionConflictException, XmlParserException, ServerException, InvalidResponseException {
+        boolean isExist = bucketExists(bucketName);
         if(!isExist) {
-            minioClient.makeBucket(bucketName);
+            makeBucket(bucketName);
             System.out.println(bucketName + " Bucket created.");
         }
     }
 
-    private static void createBucket() throws IOException, InvalidKeyException, NoSuchAlgorithmException, InsufficientDataException, InternalException, NoResponseException, InvalidBucketNameException, XmlPullParserException, ErrorResponseException, RegionConflictException {
+    private static boolean bucketExists(String bucketName) throws IOException, InvalidKeyException, InvalidResponseException, InsufficientDataException, NoSuchAlgorithmException, ServerException, InternalException, XmlParserException, InvalidBucketNameException, ErrorResponseException {
+        return minioClient.bucketExists(BucketExistsArgs.builder().bucket(bucketName).build());
+    }
+
+    private static void createBucket() throws IOException, InvalidKeyException, NoSuchAlgorithmException, InsufficientDataException, InternalException,  InvalidBucketNameException,  ErrorResponseException, RegionConflictException, XmlParserException, ServerException, InvalidResponseException {
         for(char start = 'a';start<='z';start++){
             String bucketName = ""+start+start+start;
-            boolean isExist = minioClient.bucketExists(bucketName);
+            boolean isExist = bucketExists(bucketName);
             if(!isExist) {
-                minioClient.makeBucket(bucketName);
+                makeBucket(bucketName);
                 System.out.println(bucketName + " Bucket created.");
             }
         }
     }
 
-    private static void removeBucket() throws IOException, InvalidKeyException, NoSuchAlgorithmException, InsufficientDataException, InternalException, NoResponseException, InvalidBucketNameException, XmlPullParserException, ErrorResponseException, RegionConflictException, InvalidArgumentException {
+    private static void removeBucket() throws IOException, InvalidKeyException, NoSuchAlgorithmException, InsufficientDataException, InternalException,  InvalidBucketNameException,  ErrorResponseException, RegionConflictException, InvalidArgumentException, XmlParserException, ServerException, InvalidResponseException {
         // 使用putObject上传一个文件到存储桶中。
         for(char start = 'a';start<='z';start++){
             String bucketName = ""+start+start+start;
@@ -188,13 +188,13 @@ public class FileUploader {
         }
     }
 
-    private static void forceRemoveBucket(String bucketName) throws IOException, InvalidKeyException, NoSuchAlgorithmException, InsufficientDataException, InternalException, NoResponseException, InvalidBucketNameException, XmlPullParserException, ErrorResponseException, RegionConflictException, InvalidArgumentException {
+    private static void forceRemoveBucket(String bucketName) throws IOException, InvalidKeyException, NoSuchAlgorithmException, InsufficientDataException, InternalException,  InvalidBucketNameException,  ErrorResponseException, RegionConflictException, InvalidArgumentException, XmlParserException, ServerException, InvalidResponseException {
         minioClient.removeBucket(bucketName);
         System.out.println("Bucket removed.");
     }
 
-    private static void removeBucket(String bucketName) throws IOException, InvalidKeyException, NoSuchAlgorithmException, InsufficientDataException, InternalException, NoResponseException, InvalidBucketNameException, XmlPullParserException, ErrorResponseException, RegionConflictException, InvalidArgumentException {
-        boolean isExist = minioClient.bucketExists(bucketName);
+    private static void removeBucket(String bucketName) throws IOException, InvalidKeyException, NoSuchAlgorithmException, InsufficientDataException, InternalException,  InvalidBucketNameException,  ErrorResponseException, RegionConflictException, InvalidArgumentException, XmlParserException, ServerException, InvalidResponseException {
+        boolean isExist = bucketExists(bucketName);
         if(isExist) {
             Iterable<Result<Item>> myObjects = minioClient.listObjects(bucketName);
             for (Result<Item> result : myObjects) {
@@ -206,7 +206,7 @@ public class FileUploader {
         }
     }
 
-    private static void getObject(String bucketName,String objectName) throws XmlPullParserException, NoSuchAlgorithmException, InvalidKeyException, IOException {
+    private static void getObject(String bucketName,String objectName) throws NoSuchAlgorithmException, InvalidKeyException, IOException {
         try {
             long start = System.currentTimeMillis();
             minioClient.getObject(bucketName,objectName);
@@ -216,7 +216,7 @@ public class FileUploader {
         }
     }
 
-    private static void getObject() throws XmlPullParserException, NoSuchAlgorithmException, InvalidKeyException, IOException {
+    private static void getObject() throws  NoSuchAlgorithmException, InvalidKeyException, IOException {
         try {
             InputStream is = minioClient.getObject("test","test.png");
             writeToLocal(is,"E:\\Tmp\\copy.jpg");
@@ -239,7 +239,7 @@ public class FileUploader {
         os.flush();
         os.close();
     }
-    private static void listObjects() throws XmlPullParserException, NoSuchAlgorithmException, InvalidKeyException, IOException {
+    private static void listObjects() throws  NoSuchAlgorithmException, InvalidKeyException, IOException {
         try {
             // 检查存储桶是否已经存在
             boolean isExist = minioClient.bucketExists("test");
@@ -257,18 +257,18 @@ public class FileUploader {
 
     }
 
-    private static void putObject(String bucketName) throws XmlPullParserException, NoSuchAlgorithmException, InvalidKeyException, IOException {
+    private static void putObject(String bucketName) throws  NoSuchAlgorithmException, InvalidKeyException, IOException {
         try {
             for(char start = 'a';start<='c';start++){
                 String objectName = ""+start+start+start+".png";
-                boolean isExist = minioClient.bucketExists(bucketName);
+                boolean isExist = minioClient.bucketExists(BucketExistsArgs.builder().bucket(bucketName).build());
                 if(isExist) {
                     System.out.println("Bucket already exists.");
                 } else {
                     // 创建一个名为asiatrip的存储桶，用于存储照片的zip文件。
-                    minioClient.makeBucket(bucketName);
+                    makeBucket(bucketName);
                 }
-                minioClient.putObject(bucketName,objectName, TEST_PNG_FILE);
+                uploadObject(bucketName,objectName, TEST_PNG_FILE);
             }
             System.out.println("E:\\Tmp\\test.jpg is successfully uploaded as test.jpg to `test` bucket.");
         } catch(MinioException e) {
@@ -276,18 +276,22 @@ public class FileUploader {
         }
     }
 
-    private static void putObject() throws XmlPullParserException, NoSuchAlgorithmException, InvalidKeyException, IOException {
+    private static void uploadObject(String bucketName, String objectName, String filePath) throws IOException, InvalidKeyException, InvalidResponseException, InsufficientDataException, NoSuchAlgorithmException, ServerException, InternalException, XmlParserException, InvalidBucketNameException, ErrorResponseException {
+        minioClient.uploadObject(UploadObjectArgs.builder().bucket(bucketName).object(objectName).filename(filePath).build());
+    }
+
+    private static void putObject() throws  NoSuchAlgorithmException, InvalidKeyException, IOException {
         try {
             for(char start = 'a';start<='z';start++){
                 String bucketName = ""+start+start+start;
-                boolean isExist = minioClient.bucketExists(bucketName);
+                boolean isExist = minioClient.bucketExists(BucketExistsArgs.builder().bucket(bucketName).build());
                 if(isExist) {
                     System.out.println("Bucket already exists.");
                 } else {
                     // 创建一个名为asiatrip的存储桶，用于存储照片的zip文件。
-                    minioClient.makeBucket(bucketName);
+                    makeBucket(bucketName);
                 }
-                minioClient.putObject(bucketName,bucketName+".png", TEST_PNG_FILE);
+                uploadObject(bucketName,bucketName+".png", TEST_PNG_FILE);
             }
             System.out.println("E:\\Tmp\\test.jpg is successfully uploaded as test.jpg to `test` bucket.");
         } catch(MinioException e) {
@@ -295,17 +299,17 @@ public class FileUploader {
         }
     }
 
-    private static void putObject(String bucketName,String objectName) throws XmlPullParserException, NoSuchAlgorithmException, InvalidKeyException, IOException {
+    private static void putObject(String bucketName,String objectName) throws NoSuchAlgorithmException, InvalidKeyException, IOException {
         try {
-            boolean isExist = minioClient.bucketExists(bucketName);
+            boolean isExist = minioClient.bucketExists(BucketExistsArgs.builder().bucket(bucketName).build());
             if(isExist) {
                 System.out.println("Bucket already exists.");
             } else {
                 // 创建一个名为asiatrip的存储桶，用于存储照片的zip文件。
-                minioClient.makeBucket(bucketName);
+                makeBucket(bucketName);
             }
             long start =System.currentTimeMillis();
-            minioClient.putObject(bucketName,objectName, "E:\\Tmp\\2020102113352173404.pdf");
+            uploadObject(bucketName,objectName, "E:\\Tmp\\2020102113352173404.pdf");
             System.out.println(System.currentTimeMillis() - start);
         } catch(MinioException e) {
             System.out.println("Error occurred: " + e);
