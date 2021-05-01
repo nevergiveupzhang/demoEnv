4fdea594bfacd514a4e3bc4f0265fa9ce1df2105
nevergiveupzhang <zhtao01@163.com>
2021-05-01 13:14:04 +0800

    netty

--- JavaSEDemo/src/com/example/demo/java/test/Test.java
+++ JavaSEDemo/src/com/example/demo/java/test/Test.java
@@ -8,9 +8,10 @@ import java.util.concurrent.atomic.AtomicInteger;
 public class Test {
     private int a = 9;
     public static void main(String[] args ) throws InterruptedException, NoSuchFieldException, IllegalAccessException {
-        Test test = new Test();
-        Field f = Test.class.getDeclaredField("a");
-        f.setAccessible(true);
-        System.out.println(f.get(test));
+
+    }
+
+    public void james(int a){
+
     }
 }
